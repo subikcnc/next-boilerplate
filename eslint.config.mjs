e073bbfebdb4d2000554f1816ee9ba3b6100afd9
cnc <subik@curvesncolors.ch>
2025-06-15 10:40:36 +0545

    Boilerplate first commit

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,6 +1,9 @@
 import { dirname } from "path";
 import { fileURLToPath } from "url";
+
 import { FlatCompat } from "@eslint/eslintrc";
+import eslintPluginImport from 'eslint-plugin-import';
+
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
@@ -10,7 +13,44 @@ const compat = new FlatCompat({
 });
 
 const eslintConfig = [
-  ...compat.extends("next/core-web-vitals", "next/typescript"),
+  ...compat.extends("next/core-web-vitals", "next/typescript", 'prettier'),{
+    files: ['**/*.{js,cjs,mjs,ts,tsx}'],
+    ignores: ['components/ui/'],
+    plugins: {
+      import: eslintPluginImport,
+    },
+    rules: {
+      'camelCase': 'off',
+      'import/order': [
+        'error',
+        {
+          groups: [
+            'builtin',
+            'external',
+            'internal',
+            ['parent', 'sibling'],
+            'index',
+            'object',
+          ],
+          'newlines-between': 'always',
+          pathGroups: [
+            {
+              pattern: '@app/',
+              group: 'external',
+              position: 'after',
+            },
+          ],
+          pathGroupsExcludedImportTypes: ['builtin'],
+          alphabetize: {
+            order: 'asc',
+            caseInsensitive: true,
+          },
+        },
+        
+      ],
+      'no-undef': 'off', // Needed if using TypeScript
+    },
+  },
 ];
 
 export default eslintConfig;
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,9 @@
 import Image from "next/image";
 
+import { Button } from "@/components/ui/button";
+
+
+
 export default function Home() {
   return (
     <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
@@ -97,6 +101,7 @@ export default function Home() {
           />
           Go to nextjs.org →
         </a>
+        <Button>Button</Button>
       </footer>
     </div>
   );
